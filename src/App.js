91fd8bea735088e3b4c8036f43d21b61de556aa8
eungyeong2 <eungyeong86@gmail.com>
2023-05-11 15:49:09 +0900

    NewsViewer 구현 - axios로 API 호출해서 데이터 받아오기 - 데이터 연동하기 - 카테고리 기능 구현하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from 'react';
+import axios from '../node_modules/axios/index';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
-function App() {
+const App = () => {
+  // const [data, setData] = useState();
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+  // const onClick = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       'https://newsapi.org/v2/top-headlines?country=kr&category=technology&apiKey=88c2753ff425461ba70edf97a763a6be',
+  //     );
+
+  //     setData(response.data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div>
+    //   <div>
+    //     <button onClick={onClick}>불러오기</button>
+    //   </div>
+    //   {data && (
+    //     <textarea
+    //       rows={7}
+    //       value={JSON.stringify(data, null, 2)}
+    //       readOnly={true}
+    //     />
+    //   )}
+    // </div>
+    // <hr></hr>
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
   );
-}
+};
 
 export default App;
