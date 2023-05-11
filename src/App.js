18017fd19f734af19600789ceecd0c6414c3674f
eungyeong2 <eungyeong86@gmail.com>
2023-05-11 16:07:47 +0900

    Router 적용

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
-import { useCallback, useState } from 'react';
-import axios from '../node_modules/axios/index';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import { Route, Routes } from '../node_modules/react-router-dom/dist/index';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  // const [data, setData] = useState();
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => setCategory(category), []);
-  // const onClick = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       'https://newsapi.org/v2/top-headlines?country=kr&category=technology&apiKey=88c2753ff425461ba70edf97a763a6be',
-  //     );
-
-  //     setData(response.data);
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
   return (
-    // <div>
-    //   <div>
-    //     <button onClick={onClick}>불러오기</button>
-    //   </div>
-    //   {data && (
-    //     <textarea
-    //       rows={7}
-    //       value={JSON.stringify(data, null, 2)}
-    //       readOnly={true}
-    //     />
-    //   )}
-    // </div>
-    // <hr></hr>
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
+    // 기존 return 값
+    // <>
+    //   <Categories category={category} onSelect={onSelect} />
+    //   <NewsList category={category} />
+    // </>
+
+    // Router 적용
+    <Routes>
+      <Route path="/" element={<NewsPage />} />
+      <Route path="/:category" element={<NewsPage />} />
+    </Routes>
   );
 };
 
